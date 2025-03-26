b2545ad17e58a2550fbd1cec1e5ba36b783fafc0
iStrangeGT <fixlegend1@gmail.com>
2025-03-26 17:36:31 +0700

    KHJHG

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,7 +6,7 @@ const Register = () => {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-blue-800 md:text-2xl ">
               Create an account
             </h1>
             <form className="space-y-4 md:space-y-6">
@@ -37,7 +37,7 @@ const Register = () => {
                   type="email"
                   name="email"
                   id="email"
-                  placeholder="name@company.com"
+                  placeholder="Enter Your Email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                   required
                 />
@@ -60,7 +60,7 @@ const Register = () => {
               </div>
               <button
                 type="submit"
-                className="w-full text-gray-700 bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                className="w-full text-white  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 Create an account
               </button>
